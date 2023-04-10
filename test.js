0723e7a4e1828af2de15ba88dba309a81f135dfd
halfdreamt <31080937+halfdreamt@users.noreply.github.com>
2023-04-09 21:53:07 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const app = require('../app');
+const request = require('supertest')(app);
+
+describe('GET /', function() {
+    it('should return the login.ejs view', function(done) {
+      request.get('/')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          assert.equal(res.text, 'login.ejs');
+          done();
+        });
+    });
+  });
+  
