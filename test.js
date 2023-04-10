ddea0f288b876f0f666b5ea7415bd19860db2346
halfdreamt <31080937+halfdreamt@users.noreply.github.com>
2023-04-09 22:48:07 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,14 +3,16 @@ const app = require('./app');
 const request = require('supertest')(app);
 
 describe('GET /', function() {
-    it('should return the login.ejs view', function(done) {
-      request.get('/')
-        .expect(200)
-        .end(function(err, res) {
-          if (err) return done(err);
-          assert.equal(res.text, 'login.ejs');
-          done();
-        });
-    });
+  it('should return the login.ejs view', function(done) {
+    request.get('/')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        // Check for the unique input element with ID 'username'
+        assert(res.text.includes('<input type="text" class="form-control" id="username" name="username" required>'));
+        // Alternatively, check for the 'Login' text within an <h1> tag
+        // assert(res.text.includes('<h1 class="text-center my-4">Login</h1>'));
+        done();
+      });
   });
-  
+});
